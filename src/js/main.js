db3ad672cff74ebd418489c93f411494e3574987
Johnny Sanchez <johnny062999@gmail.com>
2022-06-07 13:39:20 -0400

    final commit

--- src/js/main.js
+++ src/js/main.js
@@ -12,8 +12,7 @@ gsap.from('.container h1', {
     scrollTrigger: {
         trigger: ".container h1",
         
-        start: "bottom 80%",
-        markers: true
+        start: "bottom 100%",
     },
     x: "100%",
     opacity: 0,
@@ -22,6 +21,14 @@ gsap.from('.container h1', {
 })
 
 gsap.from('.container .sub-heading', {
+    onStart: function (){
+            console.log('trigger animation')
+        },
+    scrollTrigger: {
+        trigger: ".container .sub-heading",
+        
+        start: "bottom 100%",
+    },
     x: "-100%",
     opacity: 0,
     duration: 1,
@@ -29,10 +36,20 @@ gsap.from('.container .sub-heading', {
 })
 
 gsap.from('.container .order', {
+    onStart: function (){
+            console.log('trigger animation')
+        },
+    scrollTrigger: {
+        trigger: ".container .order",
+        
+        start: "bottom 100%",
+    },
     x: "100%",
     opacity: 0,
     duration: 1,
     delay: 2,
+    transition: 0,
+    clearProps: 'all',
 })
 // {
 //     // scrollTrigger: " img",
@@ -112,31 +129,39 @@ gsap.from('.versatile .product-description', {
 
 gsap.from('.exo .btn', {
     scrollTrigger: ".exo .btn",
-    y: "-20%",
+    y: "-50%",
     opacity: 0,
     duration: 1,
-    delay: 1
+    delay: 1,
+    transition: 0,
+    clearProps: 'all',
 })
 gsap.from('.ultra .btn', {
     scrollTrigger: ".ultra .btn",
-    y: "-20%",
+    y: "-50%",
     opacity: 0,
     duration: 1,
-    delay: 1
+    delay: 1,
+    transition: 0,
+    clearProps: 'all',
 })
 gsap.from('.armor .btn', {
     scrollTrigger: ".armor .btn",
-    y: "-20%",
+    y: "-50%",
     opacity: 0,
     duration: 1,
-    delay: 1
+    delay: 1,
+    transition: 0,
+    clearProps: 'all',
 })
 gsap.from('.versatile .btn', {
     scrollTrigger: ".versatile .btn",
-    y: "-20%",
+    y: "-50%",
     opacity: 0,
     duration: 1,
-    delay: 1
+    delay: 1,
+    transition: 0,
+    clearProps: 'all',
 })
 
 gsap.from('.exo img', {
@@ -149,6 +174,48 @@ gsap.from('.exo img', {
 
 gsap.from('.exo img', {
     scrollTrigger: ".exo img",
+    opacity: 0,
+    // rotation: "360",
+    duration: 3.5
+})
+gsap.from('.ultra img', {
+    scrollTrigger: ".ultra img",
+    ease: "elastic.out(1, 1)",
+    x: "50%",
+    // rotation: "360",
+    duration: 3
+})
+
+gsap.from('.ultra img', {
+    scrollTrigger: ".ultra img",
+    opacity: 0,
+    // rotation: "360",
+    duration: 3.5
+})
+gsap.from('.armor img', {
+    scrollTrigger: ".armor img",
+    ease: "elastic.out(1, 1)",
+    x: "50%",
+    // rotation: "360",
+    duration: 3
+})
+
+gsap.from('.armor img', {
+    scrollTrigger: ".armor img",
+    opacity: 0,
+    // rotation: "360",
+    duration: 3.5
+})
+gsap.from('.versatile img', {
+    scrollTrigger: ".versatile img",
+    ease: "elastic.out(1, 1)",
+    x: "50%",
+    // rotation: "360",
+    duration: 3
+})
+
+gsap.from('.versatile img', {
+    scrollTrigger: ".versatile img",
     opacity: 0,
     // rotation: "360",
     duration: 3.5
